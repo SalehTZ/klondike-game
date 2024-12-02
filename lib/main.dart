2b8a96f2a14af0b6c58ba17736b6a88866e24eb0
SalehTZ <31685655+SalehTZ@users.noreply.github.com>
2024-12-02 23:46:17 +0330

    added other objets

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
+import 'package:battle_sim/klondike_game.dart';
 import 'package:flame/game.dart';
 import 'package:flutter/widgets.dart';
 
 void main() {
-  final game = FlameGame();
+  final game = KlondikeGame();
   runApp(GameWidget(game: game));
 }
